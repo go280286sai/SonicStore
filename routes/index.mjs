bd6f0839d808d6b3add1b0a32da740ad98dec493
go280286sai <go280286sai@gmail.com>
2025-04-28 13:29:05 +0300

    fix views: rename to njk

--- routes/index.mjs
+++ routes/index.mjs
@@ -3,19 +3,19 @@ const router = Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sonic Store', active: 'home' });
+  res.render('index.njk', { title: 'Sonic Store', active: 'home' });
 });
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'ABOUT', active: 'about' });
+  res.render('about.njk', { title: 'ABOUT', active: 'about' });
 });
 router.get('/products', function(req, res, next) {
-  res.render('products', { title: 'OUR PRODUCTS', active: 'products' });
+  res.render('products.njk', { title: 'OUR PRODUCTS', active: 'products' });
 });
 router.get('/videogames', function(req, res, next) {
-  res.render('video', { title: 'VIDEO GAMES', active: 'video' });
+  res.render('video.njk', { title: 'VIDEO GAMES', active: 'video' });
 });
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'CONTACT US', active: 'contact' });
+  res.render('contact.njk', { title: 'CONTACT US', active: 'contact' });
 });
 
 
--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import pug from 'pug';
+import nunjucks from 'nunjucks';
 
 import usersRouter from './routes/users.mjs';
 import indexRouter from './routes/index.mjs';
@@ -14,14 +14,17 @@ const port = process.env.PORT || 3000;
 const address = process.env.HOST || '0.0.0.0';
 
 const app = express();
-
+// Настройка Nunjucks
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
 // Получаем __dirname для ES-модулей
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // view engine setup
 app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+app.set('view engine', 'njk');
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -36,16 +39,14 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.env = req.app.get('env'); // <<< добавить эту строку
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 app.listen(port, address, () => {
   console.log(`Example app listening on http://${address}:${port}`);
